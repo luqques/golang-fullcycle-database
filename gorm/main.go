5aa4cc6ef6412255fb1480528b35bb7e0c5909d7
Lucas da Cunha <lucas.da.cunha.dev@gmail.com>
2026-02-28 15:33:53 -0300

    usando gorm.Model com soft delete

--- gorm/main.go
+++ gorm/main.go
@@ -11,10 +11,11 @@ type Product struct {
 	ID    uint `gorm:"primaryKey"`
 	Name  string
 	Price float64
+	gorm.Model
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
